a31c7d5386e894cf5bc18b51cac4341f92d9eea0
leeexiu <leeexiu@gmail.com>
2022-11-08 16:45:07 +0900

    1

--- webpack.config.js
+++ webpack.config.js
@@ -1,80 +1,65 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
   entry: {
     // admin: './src/components/admin-controller.js'
-   main: './src/components/main.js',
-  //  board: './src/components/board.js',
-  //  index: './src/components/router/index.js',
-  //  proj: './src/project.js',
-  //  stat: './src/stat.js',
-  //  scmngt: './src/schedulemngt.js',
-  //  orzchart: './src/organizationchart.js',
+    main: "./src/components/main.js"
+    //  board: './src/components/board.js',
+    //  index: './src/components/router/index.js',
+    //  proj: './src/project.js',
+    //  stat: './src/stat.js',
+    //  scmngt: './src/schedulemngt.js',
+    //  orzchart: './src/organizationchart.js',
 
-   //erp: './src/erp.js',
+    //erp: './src/erp.js',
   },
-    devServer: {
-      // port: 3000,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8080',
-                ws: true,
-                changeOrigin: true
-            }
-        }
-      },
-    // chainWebpack: config => {
-      // config.module.rules.delete('eslint');
+  devServer: {
+    // port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  },
+  // chainWebpack: config => {
+  // config.module.rules.delete('eslint');
   // },
-  
+
   output: {
-    path: path.resolve(__dirname, './dist/'),
-    publicPath: '/vue-controller/',
-    filename: '[name]-controller.min.js'
+    path: path.resolve(__dirname, "./dist/"),
+    publicPath: "/vue-controller/",
+    filename: "[name]-controller.min.js"
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ],
+        use: ["vue-style-loader", "css-loader"]
       },
       {
         test: /\.scss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader'
-        ],
+        use: ["vue-style-loader", "css-loader", "sass-loader"]
       },
       {
         test: /\.sass$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader?indentedSyntax'
-        ],
+        use: ["vue-style-loader", "css-loader", "sass-loader?indentedSyntax"]
       },
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
+        loader: "vue-loader",
         options: {
           loaders: {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': [
-              'vue-style-loader',
-              'css-loader',
-              'sass-loader'
-            ],
-            'sass': [
-              'vue-style-loader',
-              'css-loader',
-              'sass-loader?indentedSyntax'
+            scss: ["vue-style-loader", "css-loader", "sass-loader"],
+            sass: [
+              "vue-style-loader",
+              "css-loader",
+              "sass-loader?indentedSyntax"
             ]
           }
           // other vue-loader options go here
@@ -82,41 +67,41 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '[name].[ext]?[hash]'
+          name: "[name].[ext]?[hash]"
         }
       }
     ]
   },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      vue$: "vue/dist/vue.esm.js"
     },
-    extensions: ['*', '.js', '.vue', '.json']
+    extensions: ["*", ".js", ".vue", ".json"]
   },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    overlay: true
-  },
+  // devServer: {
+  //   historyApiFallback: true,
+  //   noInfo: true,
+  //   overlay: true
+  // },
   performance: {
     hints: false
   },
-  devtool: '#eval-source-map'
-}
+  devtool: "#eval-source-map"
+};
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+if (process.env.NODE_ENV === "production") {
+  module.exports.devtool = "#source-map";
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
-      'process.env': {
+      "process.env": {
         NODE_ENV: '"production"'
       }
     }),
@@ -129,5 +114,5 @@ if (process.env.NODE_ENV === 'production') {
     new webpack.LoaderOptionsPlugin({
       minimize: false
     })
-  ])
+  ]);
 }
